50290f9d866932bc720615d57f63cadb992011a1
onifs10 <onifadebolu64@gmail.com>
2020-12-30 22:43:39 +0100

    hooks -- useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
 const App = () => {
-  return React.createElement(
-    "div", // html-element  || component created with react
-    { id: "test" }, // attributes
-    React.createElement("h1", {}, "Testing"), // children of the component
-    React.createElement(Pet, {
-      name: "luna",
-      animal: "dog",
-      breed: "sdsfs",
-    }),
-    React.createElement(Pet, {
-      name: "luna2",
-      animal: "snake",
-      breed: "sdsfs",
-    }),
-    React.createElement(Pet, {
-      name: "luna3",
-      animal: "cat",
-      breed: "mixed",
-    })
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me</h1>
+      <SearchParams />
+    </div>
   );
 };
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
